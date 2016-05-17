e1a30395e9f09679b27abfa36949899803c61752
Vitaliy Ognev <vitaliy.ognev@gmail.com>
2016-05-17 21:40:33 +0300

    indent

--- make.sh
+++ make.sh
@@ -6,8 +6,8 @@ set -e
 TAG=${TAG:-latest}
 
 if [ "$#" == "0" ]; then
-	echo "Usage: $0 <tar> <image> <test> <run> <push>"
-	exit 1
+  echo "Usage: $0 <tar> <image> <test> <run> <push>"
+  exit 1
 fi
 
 # base packages to be installed
@@ -27,62 +27,62 @@ APT_CONFIG="etc/apt/apt.conf"
 
 # shorthand for apt-get with our layout
 function aptget() {
-	APT_CONFIG=$APT_CONFIG apt-get -o Dir=$root \
-	  -o Dir="$root" \
-	  -o Dir::State::status="$root/var/lib/dpkg/status" \
-	  $@
+  APT_CONFIG=$APT_CONFIG apt-get -o Dir=$root \
+    -o Dir="$root" \
+    -o Dir::State::status="$root/var/lib/dpkg/status" \
+    $@
 }
 
 while test $# -gt 0; do
 case "$1" in
-	"tar" )
-		# cleanup
-		mkdir -p $root; rm -rf $root/*
+  "tar" )
+    # cleanup
+    mkdir -p $root; rm -rf $root/*
 
-		# make skel
-		mkdir -p $root/etc/apt/apt.conf.d/
-		mkdir -p $root/etc/apt/preferences.d/
-		mkdir -p $root/etc/apt/sources.list.d/
-		mkdir -p $root/etc/apt/trusted.gpg.d/
-		mkdir -p $root/var/cache/apt/archives/partial
-		mkdir -p $root/var/lib/dpkg
-		touch $root/var/lib/dpkg/status
+    # make skel
+    mkdir -p $root/etc/apt/apt.conf.d/
+    mkdir -p $root/etc/apt/preferences.d/
+    mkdir -p $root/etc/apt/sources.list.d/
+    mkdir -p $root/etc/apt/trusted.gpg.d/
+    mkdir -p $root/var/cache/apt/archives/partial
+    mkdir -p $root/var/lib/dpkg
+    touch $root/var/lib/dpkg/status
 
-		cp etc/apt/sources.list etc/apt/apt.conf $root/etc/apt/
+    cp etc/apt/sources.list etc/apt/apt.conf $root/etc/apt/
 
-		# fetch fresh packages database
-		aptget update
+    # fetch fresh packages database
+    aptget update
 
-		# get uris of debs and cache them
-		aptget --print-uris --yes install $base | cut -d\' -f2 | grep http:// > list.txt
-		wget -P $root/var/cache/apt/archives -i list.txt && rm list.txt
+    # get uris of debs and cache them
+    aptget --print-uris --yes install $base | cut -d\' -f2 | grep http:// > list.txt
+    wget -P $root/var/cache/apt/archives -i list.txt && rm list.txt
 
-		# unpack debs to $root
-		for pkg in $root/var/cache/apt/archives/*.deb; do 
-		    echo `basename "$pkg"`
-		    dpkg-deb --fsys-tarfile "$pkg" | tar -C $root -xpf -
-		done
+    # unpack debs to $root
+    for pkg in $root/var/cache/apt/archives/*.deb; do 
+        echo `basename "$pkg"`
+        dpkg-deb --fsys-tarfile "$pkg" | tar -C $root -xpf -
+    done
 
-		# settle postinstall script
-		cp bin/postinst.sh $root/ && chmod +x $root/postinst.sh
+    # settle postinstall script
+    cp bin/postinst.sh $root/ && chmod +x $root/postinst.sh
 
-		./bin/chroot.sh $root /postinst.sh
+    ./bin/chroot.sh $root /postinst.sh
 
-		cd root; tar czf ../root.tar.gz .
-		;;
-	"image" )
-		docker rmi vognev/base || true
-		docker build --build-arg http_proxy=$http_proxy -f Dockerfile.base -t vognev/base:$TAG .
-		;;
-	"run" )
-		docker run -it --rm --entrypoint /bin/bash vognev/base:$TAG
-		;;
-	"push" )
-		docker push vognev/base:$TAG
-		;;
-	"test" )
-		# todo
-		;;
+    cd root; tar czf ../root.tar.gz .
+    ;;
+  "image" )
+    docker rmi vognev/base || true
+    docker build --build-arg http_proxy=$http_proxy -f Dockerfile.base -t vognev/base:$TAG .
+    ;;
+  "run" )
+    docker run -it --rm --entrypoint /bin/bash vognev/base:$TAG
+    ;;
+  "push" )
+    docker push vognev/base:$TAG
+    ;;
+  "test" )
+    # todo
+    ;;
 esac
 
 shift
